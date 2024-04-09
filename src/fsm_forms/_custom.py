4b211cbf69db2875a16a8124566198845525d4af
fry1231 <sobaka.samsobaka@yandex.ru>
2024-04-09 02:26:57 +0200

    [Fix] await to create_task in fsm

--- src/fsm_forms/_custom.py
+++ src/fsm_forms/_custom.py
@@ -32,4 +32,4 @@ class CustomState(State):
         user_id = types.User.get_current().id
         full_state_name = f'{group_name}:{state_index}:{state_name}'
         await add_user_to_state(full_state_name, user_id)
-        await delayed(120, remove_user_state, {'user_id': user_id, 'from_state': full_state_name})
+        asyncio.create_task(delayed(120, remove_user_state, {'user_id': user_id, 'from_state': full_state_name}))
